56009bd62daee0048bdad3f7c32aa0697ba5f515
SmaQu <maciej.smolik95@gmail.com>
2018-01-02 18:00:02 +0100

    Add text to about app., create about me.

--- app/src/main/java/com/example/smaqu/mypersonalcv/view/AboutMeActivity.java
+++ app/src/main/java/com/example/smaqu/mypersonalcv/view/AboutMeActivity.java
@@ -1,7 +1,10 @@
 package com.example.smaqu.mypersonalcv.view;
 
 import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.TextView;
 
 import com.example.smaqu.mypersonalcv.R;
 
@@ -11,5 +14,16 @@ public class AboutMeActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_about_me);
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+
+        ((TextView)findViewById(R.id.text_view_activity_about_me_about_me)).setText(R.string.about_me);
+        ((TextView)findViewById(R.id.text_view_activity_about_me_signature)).setText(R.string.signature);
     }
 }
